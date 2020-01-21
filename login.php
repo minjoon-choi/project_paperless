68e6b9e2613a8b91c017596e0f73d0f401f7df97
minjoon_choi <mjchois@gmail.com>
2020-01-21 22:59:45 +0900

    commit login.php

--- login.php
+++ login.php
@@ -1,11 +1,24 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<META charset = utf-8>
+<META charset = "utf-8">
+<title> 로그인 - Paperless</title>
 </head>
 
 <body>
 <h1> hello world </h1>
+
+<table style ="width:30%">
+<tr>
+<th>기간</th>
+<th>이름</th>
+</tr>
+<tr>
+<td>2019-08</td>
+<td>최민준</td>
+</tr>
+</table>
+
 </body>
 </html>
 
